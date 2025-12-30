56d90dd0fadc2a8f3e9dbbcd986e09d23745d61f
Adrian Vigil <insightvigil@gmail.com>
2025-12-29 23:58:18 -0600

    first commit

---
+++ server/src/models/planets.model.js
@@ -0,0 +1,57 @@
+
+const {createReadStream} = require('fs');
+const path = require('path');
+const {parse} = require('csv-parse');
+
+const habitablePlanets = [];
+
+function isHabitablePlanet(planet) {
+    return planet['koi_disposition']==='CONFIRMED' 
+    && planet['koi_insol'] > 0.36 && planet['koi_insol'] <1.11
+    && planet['koi_prad'] < 1.6
+}
+
+/*
+const promise = new Promise((resolve, reject)=> {
+    resolve(42)
+    })
+
+    promise.then(()=> {
+        })
+
+    const result = await promise;
+    console.log(result);
+*/ 
+
+function loadPlanetsData() {
+    return new Promise((resolve,reject) => {
+        createReadStream(path.join(__dirname,'..','..','data','kepler_data.csv'))
+        .pipe(parse({
+            comment: '#',
+            columns: true
+        }))
+        .on('data', (data)=> {
+            if(isHabitablePlanet(data)){
+                habitablePlanets.push(data);
+            }
+            
+        })
+        .on('error', (error)=>{ 
+            console.log(error);
+            reject(err);
+        }
+        )
+        .on('end',()=> {
+            console.log(habitablePlanets.length + ' habitable planets found!🌎');
+            resolve();
+            console.log('done');
+        });
+    });
+
+}
+
+
+    module.exports = {
+       planets: habitablePlanets,
+       loadPlanetsData
+    }
