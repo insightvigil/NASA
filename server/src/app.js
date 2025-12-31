722a3a61e31536c8365855c4077242e64576c879
Jose Adrian Flores Vigil <insightvigil@gmail.com>
2025-12-31 01:00:03 -0600

    Deployment Optimized

--- server/src/app.js
+++ server/src/app.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const planetsRouter = require('./routes/planets/planets.router');
 const cors = require('cors');
+const path = require('path');
 
 const app = express();
 
 app.use(cors({
     origin: 'http://localhost:3000'
 }));
+
 app.use(express.json());
+app.use(express.static(path.join(__dirname,'..','public')));
 app.use('/planets',planetsRouter);
 
 
